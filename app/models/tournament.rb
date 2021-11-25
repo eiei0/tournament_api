0b60912627e453dfa0c65d0b399f3cef555d6ca0
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2021-11-25 01:02:04 -0600

    Add presence validations for essential fields
    This was done to ensure that the essential fields for a tournament to
    operate a present on creation.

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class Tournament < ApplicationRecord
+  validates :name,
+            :start_date,
+            :end_date,
+            :registration_start_date,
+            :registration_end_date,
+            presence: true
 end
--- spec/models/tournament_spec.rb
+++ spec/models/tournament_spec.rb
@@ -3,5 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Tournament, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:start_date) }
+    it { is_expected.to validate_presence_of(:end_date) }
+    it { is_expected.to validate_presence_of(:registration_start_date) }
+    it { is_expected.to validate_presence_of(:registration_end_date) }
+  end
 end
